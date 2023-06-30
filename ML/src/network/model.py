61339deac12b1dbcf7cfcfc8e0eca610a6b9791c
Johan Mylius Kroken <johan.m.kroken@gmail.com>
2023-06-30 11:10:40 +0200

    collected network source code in a single folder

---
+++ ML/src/network/model.py
@@ -0,0 +1,69 @@
+from src.MLutils import *
+from src.COW import COW
+from src.DataHandler import TestCubes
+from src.h5pySTUFF import h5Handler
+
+
+class Model:
+    def __init__(self, Net, DataPath:str):
+        self.Net = Net
+        self.DataPath = DataPath
+        ###TODO Fix the hard coding below
+        self.model = self.Net()
+        self.lossFn = nn.BCELoss()
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=0.001)
+
+    def loadData(self):
+        ###TODO Extend to more cubes per subdir
+        h5newton = h5Handler(self.DataPath+"newton/")
+        h5gr = h5Handler(self.DataPath+"gr/")
+        self.newtonCube = h5newton()[0]
+        self.grCube = h5gr()[0]
+        ###TODO Fix the hard coding below
+        self.data = TestCubes(self.newtonCube, self.grCube, transform=False, additionalInfo=True)
+        [train, val, test] = random_split(self.data, [0.6, 0.25, 0.15], generator=generator1)
+        batch_size = 32
+        self.trainLoader = DataLoader(train, batch_size=batch_size)
+        self.valLoader = DataLoader(val, batch_size=batch_size)
+        self.testLoader = DataLoader(test, batch_size=batch_size)
+
+    def train(self):
+        ###TODO Fix the hard coding below
+        nrEpochs = 5
+        for epoch in range(nrEpochs):
+            for dataObject in self.trainLoader:
+                inputs = dataObject["image"]
+                labels = dataObject["label"]
+                yPred = self.model(inputs)
+                loss = self.lossFn(yPred, labels)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+            correct = 0
+            count = 0
+            tol = 1e-7
+            for dataObject in self.valLoader:
+                inputs = dataObject["image"]
+                labels = dataObject["label"]
+                yPred = self.model(inputs)
+                correct += (torch.abs(torch.round(yPred)-labels)<tol).float().sum()
+                count += len(labels)
+            accuracy = correct / count
+            print(f"Epoch [{epoch+1}/{nrEpochs}], Loss: {loss.item():.3f}, accuracy: {accuracy*100:.2f} %")
+
+    def test(self):
+        correct = 0
+        count = 0
+        tol = 1e-7
+        for dataObject in self.testLoader:
+            inputs = dataObject["image"]
+            labels = dataObject["label"]
+            yPred = self.model(inputs)
+            correct += (torch.abs(torch.round(yPred) - labels)<tol).float().sum()
+            count += len(labels)
+        acc = correct / count
+
+        print(f"Correct: [{int(correct)}/{count}] giving accuracy of {acc*100:.2f} %")
+
+if __name__=="__main__":
+    print("Model class only")
