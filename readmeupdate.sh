98d93a73f6e149b0e2847cdc0d4a725fe37b750e
Johan Mylius Kroken <johan.m.kroken@gmail.com>
2023-06-30 10:06:39 +0200

    script to automagically update the ## Tree section of the main readme file

---
+++ readmeupdate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+file="README.md"
+
+# Set the start and end subheadings
+start_subheading="## Tree"
+end_subheading="##"
+
+# Temp file
+temp_file=$(mktemp)
+
+# Flags
+keep=true
+remove=false
+
+while IFS= read -r line; do
+    # Control the keep and remove flag
+    if [[ $line == *"$start_subheading"* ]]; then 
+        keep=false  # Stops keeping if ## Tree is encountered
+    elif [[ $line == *"$end_subheading"* ]]; then
+        remove=false # If any other subheading occurs
+        keep=true
+    fi
+    # Update or keep content
+    if ! $remove; then
+        if $keep; then 
+            echo "$line" >> "$temp_file"    # simply copy line
+        else    #   write tree output
+            echo "$start_subheading" >> "$temp_file" 
+            tree . | sed -e 's/^/   /' >> "$temp_file" 
+            echo "Updated on $(date +%F)" >> "$temp_file"
+            remove=true
+        fi
+    fi
+done < "$file"
+
+mv "$temp_file" "$file"
+    
+
