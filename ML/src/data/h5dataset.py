fed286740a9919e8414984de6a1e9f1df14b61ec
Johanmkr <johan.m.kroken@gmail.com>
2023-07-25 15:15:40 +0200

    added dataset file

---
+++ ML/src/data/h5dataset.py
@@ -0,0 +1,13 @@
+import numpy as np
+import os
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+class DatasetOfCubes:
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass 
+
+    def __getitem__(self):
