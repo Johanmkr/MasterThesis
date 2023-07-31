da416b58d34f75eba750b293c8387ce9b1a4f428
Johanmkr <johan.m.kroken@gmail.com>
2023-07-31 09:46:21 +0200

    removed wrong simulations

--- data_generation/clean_simulation.sh
+++ data_generation/clean_simulation.sh
@@ -6,7 +6,13 @@ seeds="$1"
 # Source the functions needed
 source func_lib.sh
 
-while IFS= read -r seed; do 
-	clean_up_from_seed $seed
-done < "$seeds"
+echo "Are you sure you want to delete simulations? [y/n]"
+read sure
 
+if [ "$sure" = "y" ]; then
+	while IFS= read -r seed; do 
+		clean_up_from_seed $seed
+	done < "$seeds"
+else
+	echo "Aborting..."
+fi
