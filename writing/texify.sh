34a5a4960423d02410c3869db5ca62224ee8e9fc
Johanmkr <johan.m.kroken@gmail.com>
2023-08-23 10:07:35 +0200

    generated latex files and UiO masterthesis templates

---
+++ writing/texify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Texfilename
+texfilename=$1
+
+# This script is used to compile a LaTeX file to PDF using pdflatex.
+pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
+
+mv garbage/${texfilename%.tex}.pdf ${texfilename%.tex}.pdf
