0c571c4438cf83a43215c2a39628cd7110411e72
Johanmkr <johan.m.kroken@gmail.com>
2023-08-30 10:52:17 +0200

    more organised input parameters

--- ML/src/data/cambPK.py
+++ ML/src/data/cambPK.py
@@ -3,11 +3,31 @@ import matplotlib.pyplot as plt
 import camb
 from camb import model
 
+camb_main_params = {
+    "H0": 67.556,
+    "ombh2": 0.022032,
+    "omch2": 0.12038,
+    "TCMB": 2.7255,
+    "standard_neutrino_neff": 3.046
+}
+
+camb_init_power_params = {
+    "As": 2.215e-9,
+    "ns": 0.9619,
+    "r": 0
+}
+
+camb_matter_power_params = {
+    "minkh": 1e-4,
+    "maxkh": 1,
+    "npoints": 200
+}
+
 class CambSpectra:
     def __init__(self) -> None:
         self.pars = camb.CAMBparams()
-        self.pars.set_cosmology(H0=67.556, ombh2=0.022032, omch2=0.12038, TCMB=2.7255, standard_neutrino_neff=3.046)
-        self.pars.InitPower.set_params(As=2.215e-9, ns=0.9619, r=0)
+        self.pars.set_cosmology(**camb_main_params)
+        self.pars.InitPower.set_params(camb_init_power_params)
         self.pars.set_for_lmax(2500, lens_potential_accuracy=0)
     
     def __call__(self, redshift:float=0.0):# -> tuple(np.ndarray, np.ndarray):
@@ -15,7 +35,7 @@ class CambSpectra:
         results = camb.get_results(self.pars)
         self.pars.NonLinear = model.NonLinear_both
         results.calc_power_spectra(self.pars)
-        kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1, npoints = 200)
+        kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(**camb_matter_power_params)
         return kh_nonlin, pk_nonlin[0,:]
 
 
