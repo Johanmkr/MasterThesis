ac0ae0276a4063ed78845182168dd8fe9b1aaaa5
Johanmkr <johan.m.kroken@gmail.com>
2023-09-11 11:22:28 +0200

    trying to get this to work with bibtex and automatical referencing

--- writing/texify.sh
+++ writing/texify.sh
@@ -5,5 +5,7 @@ texfilename=$1
 
 # This script is used to compile a LaTeX file to PDF using pdflatex.
 pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
+biblatex ${texfilename}
+pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
 
 mv garbage/${texfilename%.tex}.pdf ${texfilename%.tex}.pdf
