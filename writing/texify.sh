fec5475664c126d38646cb44ff585dc67b710076
Johanmkr <johan.m.kroken@gmail.com>
2023-09-11 13:18:13 +0200

    include works, references not so much yet

--- writing/texify.sh
+++ writing/texify.sh
@@ -4,8 +4,10 @@
 texfilename=$1
 
 # This script is used to compile a LaTeX file to PDF using pdflatex.
-pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
-biblatex ${texfilename}
-pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
+#pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
+pdflatex ${texfilename}
+#biblatex ${texfilename}
+#pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
 
-mv garbage/${texfilename%.tex}.pdf ${texfilename%.tex}.pdf
+#mv garbage/${texfilename%.tex}.pdf ${texfilename%.tex}.pdf
+
