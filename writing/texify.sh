dce6d265483df9a9f605213fc8604c4850045a54
Johanmkr <johan.m.kroken@gmail.com>
2023-09-25 15:42:13 +0200

    wrote some, structured document. Now tex works as it should

--- writing/texify.sh
+++ writing/texify.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 # Texfilename
-texfilename=$1
-
+#texfilename=$1
+texfilename="masterthesis"
 # This script is used to compile a LaTeX file to PDF using pdflatex.
 #pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
-pdflatex ${texfilename}
+pdflatex ${texfilename}.tex
+biber ${texfilename}
+pdflatex ${texfilename}.tex
+pdflatex ${texfilename}.tex
 #biblatex ${texfilename}
 #pdflatex --output-directory=garbage --interaction=nonstopmode ${texfilename}
 
