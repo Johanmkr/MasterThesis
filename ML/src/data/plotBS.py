1450204798a3b03ed1510c5af5dad41b368a40a7
Johanmkr <johan.m.kroken@gmail.com>
2023-09-28 18:45:38 +0200

    updated this, tested for the different plots

--- ML/src/data/plotBS.py
+++ ML/src/data/plotBS.py
@@ -4,6 +4,7 @@ from matplotlib.lines import Line2D
 from figure import CustomFigure
 import bispectrum as bs
 import powerspectra as ps
+import cube
 import pandas as pd
 
 from IPython import embed
@@ -12,17 +13,83 @@ from IPython import embed
 ## Start with some easy testing to see if things work:
 
 seed = 1234
+redshift=1
 
 datapath = "/mn/stornext/d10/data/johanmkr/simulations/gevolution_first_runs/"
 
-grBispectrum = bs.CubeBispectrum(datapath + f"seed{seed:04d}/gr/gr_000_phi.h5")
-newtonBispectrum = bs.CubeBispectrum(datapath + f"seed{seed:04d}/newton/newton_000_phi.h5")
+grBispectrum = bs.CubeBispectrum(datapath + f"seed{seed:04d}/gr/gr_{cube.redshift_to_snap[redshift]}_phi.h5")
+newtonBispectrum = bs.CubeBispectrum(datapath + f"seed{seed:04d}/newton/newton_{cube.redshift_to_snap[redshift]}_phi.h5")
 
 def plot_equilateral(k_range):
-    grBispectrum.equilateral_bispectrum(k_range).plot(x="k", y="B", label="GR")
-    newtonBispectrum.equilateral_bispectrum(k_range).plot(x="k", y="B", label="Newton")
-    plt.title("Equilateral Bispectrum")
-    plt.ylabel(r"$B(k_1, k_2, k_3)$")
-    plt.xlabel(r"$k_1 = k_2 = k_3$ [h/Mpc]")
-    plt.legend()
-    plt.show()
+    grdBk = grBispectrum.equilateral_bispectrum(k_range)
+    ndBk = newtonBispectrum.equilateral_bispectrum(k_range)
+    eqfig = CustomFigure()
+    settings = {
+                "xscale": "log",
+                "yscale": "log",
+                "xlabel": r"k $[h/Mpc]$",
+                "ylabel": r"P(k) $[h/Mpc]^3$",
+                "title": f"Equilateral Bispectrum of 'phi' at redshift z={redshift:.1f}, for seed {seed:04d}"
+            }
+    eqfig.set_settings(settings)
+
+    lines = []
+
+    lines.append(Line2D(grdBk["k"], abs(grdBk["B"]), color="blue", label="GR"))
+    lines.append(Line2D(ndBk["k"], abs(ndBk["B"]), color="red", label="Newton"))
+
+    for line in lines:
+        eqfig.ax.add_line(line)
+
+    leg1 = eqfig.ax.legend(handles=lines, loc="upper right")
+    eqfig.ax.add_artist(leg1)
+    eqfig.ax.autoscale_view()
+
+    comparison_axis_settings = {
+        # "xscale": "log",
+        "yscale": "log",
+        "ylabel": r"GR-Newton",
+    }
+    eqfig.gen_twinx(comparison_axis_settings)
+
+    eqfig.axes[1].plot(grdBk["k"], grdBk["B"] - ndBk["B"], color="black", label="GR-Newton")
+
+def plot_squeezed(k_range):
+    grdBk = grBispectrum.squeezed_bispectrum(k_range)
+    ndBk = newtonBispectrum.squeezed_bispectrum(k_range)
+    eqfig = CustomFigure()
+    settings = {
+                "xscale": "log",
+                "yscale": "log",
+                "xlabel": r"k $[h/Mpc]$",
+                "ylabel": r"P(k) $[h/Mpc]^3$",
+                "title": f"Squeezed Bispectrum of 'phi' at redshift z={redshift:.1f}, for seed {seed:04d}"
+            }
+    eqfig.set_settings(settings)
+
+    lines = []
+
+    lines.append(Line2D(grdBk["k"], abs(grdBk["B"]), color="blue", label="GR"))
+    lines.append(Line2D(ndBk["k"], abs(ndBk["B"]), color="red", label="Newton"))
+
+    for line in lines:
+        eqfig.ax.add_line(line)
+
+    leg1 = eqfig.ax.legend(handles=lines, loc="upper right")
+    eqfig.ax.add_artist(leg1)
+    eqfig.ax.autoscale_view()
+
+    comparison_axis_settings = {
+        # "xscale": "log",
+        "yscale": "log",
+        "ylabel": r"GR-Newton",
+    }
+    eqfig.gen_twinx(comparison_axis_settings)
+
+    eqfig.axes[1].plot(grdBk["k"], grdBk["B"] - ndBk["B"], color="black", label="GR-Newton")
+
+
+ks = np.geomspace(grBispectrum.kF, grBispectrum.kN, 75)
+plot_equilateral(ks)
+plot_squeezed(ks)
+plt.show()
