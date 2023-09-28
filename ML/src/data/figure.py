9da59c2e5ed54b3b5017740d28bf83f2d1fcd39b
Johanmkr <johan.m.kroken@gmail.com>
2023-09-28 18:44:49 +0200

    added some functionality

--- ML/src/data/figure.py
+++ ML/src/data/figure.py
@@ -24,12 +24,30 @@ class CustomFigure:
     def __init__(self, figsize:tuple=(10,10)) -> None:
         self.figsize = figsize
         self.fig, self.ax = plt.subplots(1, 1, figsize=self.figsize)
-
+        self.axes = [self.ax]
+        
     def __call__(self) -> tuple:
         return self.fig, self.ax
     
     def set_settings(self, settings:dict) -> None:
         self.ax.set(**settings)
+
+    def gen_twinx(self, settings:dict) -> None:
+        newax = self.ax.twinx()
+        newax.set(**settings)
+        self.axes.append(newax)
+
+    def gen_twiny(self, settings:dict) -> None:
+        newax = self.ax.twiny()
+        newax.set(**settings)
+        self.axes.append(newax)
+
+    def gen_ax(self, settings:dict) -> None:
+        newax = self.fig.add_axes(settings)
+        self.axes.append(newax)
+    
+    def update_ax_settings(self, ax, settings:dict) -> None:
+        ax.set(**settings)
 
 
 if __name__=="__main__":
