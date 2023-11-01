7e58d38a7ce264d2770a5fd016474e2bf2a4fb8a
Johanmkr <johan.m.kroken@gmail.com>
2023-11-01 14:50:16 +0100

    updt. runscripts

---
+++ masterthesis/runscripts/test_dataset.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import sys
+
+
+# add path
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+sys.path.append(parent_dir)
+
+from src.data import transforms, custom_dataset
+
+from IPython import embed
+
+
+norm = transforms.Normalise(redshifts=[1.0])
+ds = custom_dataset.CustomDataset(transform=norm, additional_info=True)
+
+embed()
