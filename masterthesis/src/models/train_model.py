d7e70508f194c9546437338d60066a31606a2a2b
Johanmkr <johan.m.kroken@gmail.com>
2023-12-04 18:28:31 +0100

    updated trainer to be a module. Not done, need to implement correct logging

--- masterthesis/src/models/train_model.py
+++ masterthesis/src/models/train_model.py
@@ -1,104 +1,121 @@
-###TODO CHECK THIS
+import numpy as np
 import torch
 import torch.nn as nn
 from IPython import embed
 
 
-def train_model(
-    model,
-    optimizer,
-    loss_fn,
-    train_loader,
-    val_loader,
-    epochs,
-    device,
-    verbose=True,
-    tol: float = 1e-2,
-):
-    """
-    Train the model.
-    """
-    # Initialize variables
-    best_loss = 1e10
-    train_losses = []
-    val_losses = []
-    train_accs = []
-    val_accs = []
+class ModelTrainer:
+    def __init__(
+        self, model, optimizer, loss_fn, device, verbose=True, writer=None, tol=1e-2
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.device = device
+        self.verbose = verbose
+        self.tol = tol
 
-    # Move model to device
-    model = model.to(device)
+        # Data storage
+        self.epoch_array = []
+        self.train_loss_array = []
+        self.train_acc_array = []
+        self.test_loss_array = []
+        self.test_acc_array = []
 
-    # Loop over epochs
-    for epoch in range(1, epochs + 1):
-        # Training
-        print(f"--- Epoch {epoch} ---")
-        model.train()
+        if writer is not None:
+            self.writer = writer
+            self.writer.add_graph(self.model, torch.zeros(model.input_size))
+
+    def train_model(
+        self,
+        train_loader,
+        test_loader,
+        epochs,
+    ):
+        """
+        Train the model.
+        """
+        # Initialize variables
+        best_loss = 1e10
+
+        # Move model to device
+        self.model = self.model.to(self.device)
+
+    def train_one_epoch(
+        self, train_loader: list | np.ndarray, epoch: int, verbose: bool = True
+    ) -> tuple[float, float]:
+        print(f"--- Epoch {epoch} ---") if verbose else None
+        self.model.train()  # Set model to training mode
         train_loss = 0.0
-        train_acc = 0.0
+        train_accuracy = 0.0
         max_batches = len(train_loader)
         for i, data in enumerate(train_loader):
             # Get the inputs
-            print(f"Training: Epoch: {epoch}, batch {i+1}/{max_batches}")
+            print(
+                f"Training: Epoch: {epoch}, batch {i+1}/{max_batches}"
+            ) if verbose else None
             images, labels = data["image"], data["label"]
-            images = images.to(device)
-            labels = labels.to(device)
+            images = images.to(self.device)
+            labels = labels.to(self.device)
 
             # Zero the parameter gradients
-            optimizer.zero_grad()
+            self.optimizer.zero_grad()
 
             # Forward + backward + optimize
-            outputs = model(images)
-            loss = loss_fn(outputs, labels)
+            outputs = self.model(images)
+            loss = self.loss_fn(outputs, labels)
             loss.backward()
-            optimizer.step()
+            self.optimizer.step()
 
             # Print statistics
             train_loss += loss.item()
-            train_acc += (abs(outputs - labels) < tol).sum().item()
+            train_accuracy += (abs(outputs - labels) < self.tol).sum().item()
+        print(
+            f"Predicted (train set): [{train_accuracy}/{len(train_loader.dataset)}]"
+        ) if verbose else None
+        return train_loss, train_accuracy
 
-        # Validation
-        model.eval()
-        val_loss = 0.0
-        val_acc = 0.0
+    def test_model(self, loader: list | np.ndarray) -> tuple[float, float]:
+        self.model.eval()
+        test_loss = 0.0
+        test_accuracy = 0.0
         with torch.no_grad():
-            for i, data in enumerate(val_loader):
+            for i, data in enumerate(loader):
                 # Get the inputs
                 images, labels = data["image"], data["label"]
-                images = images.to(device)
-                labels = labels.to(device)
+                images = images.to(self.device)
+                labels = labels.to(self.device)
 
                 # Forward
-                outputs = model(images)
-                loss = loss_fn(outputs, labels)
+                outputs = self.model(images)
+                loss = self.loss_fn(outputs, labels)
 
                 # Print statistics
-                val_loss += loss.item()
-                val_acc += (abs(outputs - labels) < tol).sum().item()
-                # embed()
-        print(f"Predicted {val_acc} out of {len(val_loader.dataset)}")
-        # Print statistics
-        train_loss /= len(train_loader.dataset)
-        val_loss /= len(val_loader.dataset)
-        train_acc /= len(train_loader.dataset)
-        val_acc /= len(val_loader.dataset)
+                test_loss += loss.item()
+                test_accuracy += (abs(outputs - labels) < self.tol).sum().item()
+        print(f"Predicted (test set): [{test_accuracy}/{len(loader.dataset)}]")
+        return test_loss, test_accuracy
 
-        train_losses.append(train_loss)
-        val_losses.append(val_loss)
-        train_accs.append(train_acc)
-        val_accs.append(val_acc)
+    def log_stuff(
+        self,
+        epoch: int,
+        train_loss: float,
+        train_accuracy: float,
+        test_loss: float,
+        test_accuracy: float,
+    ):
+        assert self.writer is not None, "Writer is None"
+        self.epoch_array.append(epoch)
+        self.train_loss_array.append(train_loss)
+        self.train_acc_array.append(train_accuracy)
+        self.test_loss_array.append(test_loss)
+        self.test_acc_array.append(test_accuracy)
 
-        if verbose:
-            print(
-                f"Epoch: {epoch}: " f"Train loss: {train_loss:.4f}",
-                f"Val loss: {val_loss:.4f}",
-                f"Train acc: {train_acc:.4f}",
-                f"Val acc: {val_acc:.4f}\n",
-            )
-
-        # Save model if validation loss has decreased
-        if val_loss < best_loss:
-            best_loss = val_loss
-            torch.save(model.state_dict(), "model.pt")
+        # Add loss and accuracy to tensorboard
+        self.writer.add_scalar("Loss", {"train": train_loss, "test": test_loss}, epoch)
+        self.writer.add_scalar(
+            "Accuracy", {"train": train_accuracy, "test": test_accuracy}, epoch
+        )
 
 
 def overfit_model(
@@ -121,10 +138,13 @@ def overfit_model(
     epoch = 0
     train_loss = 1
     data = next(iter(train_loader))
-    max_epoch = 500
+    max_epoch = 1000
     train_acc = 0.0
+    epoch_array = []
+    loss_array = []
+    acc_array = []
     # for _ in range(10):
-    while train_acc < 0.9:
+    while train_acc < 0.95:
         # Training
         print(f"--- Epoch {epoch+1} ---")
         model.train()
@@ -149,11 +169,17 @@ def overfit_model(
         # Print statistics
         train_loss /= len(images)
         train_acc = predictions / len(images)
-
         epoch += 1
+
+        # Append to arrays
+        epoch_array.append(epoch)
+        loss_array.append(train_loss)
+        acc_array.append(train_acc)
+
         print(
             f"Epoch: {epoch}: "
             f"Train loss: {train_loss:.5f}, Train acc: {train_acc:.4f}, predicted: {predictions}/{len(images)}\n"
         )
         if epoch >= max_epoch:
             break
+    return epoch_array, loss_array, acc_array
