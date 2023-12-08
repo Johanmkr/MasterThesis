252e5ecdd84f60d7330139786960d6461e77b4ae
Johanmkr <johan.m.kroken@gmail.com>
2023-12-08 16:09:51 +0100

    fixed this to be of CBA-style, added layers and prepared for training

--- masterthesis/src/models/PENGUIN.py
+++ masterthesis/src/models/PENGUIN.py
@@ -1,5 +1,7 @@
 """
     2D convolutional neural network for classification of slices of datacubes from GR/Newtonian simulations.
+    
+    
 """
 
 import torch
@@ -10,11 +12,11 @@ class PENGUIN(nn.Module):
     def __init__(
         self,
         input_size: tuple,
-        layer_param: float = 4,
-        activation=nn.LeakyReLU(),
+        layer_param: float = 64,
+        activation=nn.ReLU(),
         output_activation=nn.Sigmoid(),
         bias=False,
-        dropout=0.25,
+        dropout=0.5,
     ):
         super().__init__()
         self.input_size = input_size
@@ -23,102 +25,128 @@ class PENGUIN(nn.Module):
         self.bias = bias
         self.dropout = dropout
         self.num_channels = self.input_size[0]
+        self.conv_layers = []
+        self.fc_layers = []
 
-        # LAYER - 2D convolutional layer (num_channels, 256, 256) -> (4*layer_param, 128, 128)
+        # LAYER - 2D convolutional layer (num_channels, 256, 256) -> (layer_param, 128, 128)
         self.conv2d1 = nn.Sequential(
             nn.Conv2d(
                 self.num_channels,
-                layer_param * 4,
-                kernel_size=(3, 3),
-                stride=1,
+                layer_param,
+                kernel_size=4,
+                stride=2,
                 padding=1,
                 bias=self.bias,
             ),
-            nn.MaxPool2d(kernel_size=(2, 2)),
+            nn.BatchNorm2d(layer_param),
+            self.activation,
+        )
+        self.conv_layers.append(self.conv2d1)
+
+        # LAYER - 2D convolutional layer (layer_param, 128, 128) -> (2*layer_param, 64, 64)
+        self.conv2d2 = nn.Sequential(
+            nn.Conv2d(
+                layer_param,
+                layer_param * 2,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=self.bias,
+            ),
+            nn.BatchNorm2d(layer_param * 2),
+            self.activation,
+        )
+        self.conv_layers.append(self.conv2d2)
+
+        # LAYER - 2D convolutional layer (2*layer_param, 64, 64) -> (4*layer_param, 32, 32)
+        self.conv2d3 = nn.Sequential(
+            nn.Conv2d(
+                layer_param * 2,
+                layer_param * 4,
+                kernel_size=4,
+                stride=2,
+                padding=1,
+                bias=self.bias,
+            ),
             nn.BatchNorm2d(layer_param * 4),
             self.activation,
-            nn.Dropout(self.dropout),
         )
+        self.conv_layers.append(self.conv2d3)
 
-        # LAYER - 2D convolutional layer (4*layer_param, 128, 128) -> (6*layer_param, 64, 64)
-        self.conv2d2 = nn.Sequential(
+        # LAYER - 2D convolutional layer (4*layer_param, 32, 32) -> (6*layer_param, 16, 16)
+        self.conv2d4 = nn.Sequential(
             nn.Conv2d(
                 layer_param * 4,
                 layer_param * 6,
-                kernel_size=(3, 3),
-                stride=1,
+                kernel_size=4,
+                stride=2,
                 padding=1,
                 bias=self.bias,
             ),
-            nn.MaxPool2d(kernel_size=(2, 2)),
             nn.BatchNorm2d(layer_param * 6),
             self.activation,
-            nn.Dropout(self.dropout),
         )
+        self.conv_layers.append(self.conv2d4)
 
-        # LAYER - 2D convolutional layer (6*layer_param, 64, 64) -> (8*layer_param, 32, 32)
-        self.conv2d3 = nn.Sequential(
+        # LAYER - 2D convolutional layer (6*layer_param, 16, 16) -> (8*layer_param, 8, 8)
+        self.conv2d5 = nn.Sequential(
             nn.Conv2d(
                 layer_param * 6,
                 layer_param * 8,
-                kernel_size=(3, 3),
-                stride=1,
+                kernel_size=4,
+                stride=2,
                 padding=1,
                 bias=self.bias,
             ),
-            nn.MaxPool2d(kernel_size=(2, 2)),
             nn.BatchNorm2d(layer_param * 8),
             self.activation,
-            nn.Dropout(self.dropout),
         )
+        self.conv_layers.append(self.conv2d5)
 
-        # Layer - 2D convolutional layer (8*layer_param, 32, 32) -> (10*layer_param, 16, 16)
-        self.conv2d4 = nn.Sequential(
+        # LAYER - 2D convolutional layer (8*layer_param, 8, 8) -> (10*layer_param, 4, 4)
+        self.conv2d6 = nn.Sequential(
             nn.Conv2d(
                 layer_param * 8,
                 layer_param * 10,
-                kernel_size=(3, 3),
-                stride=1,
+                kernel_size=4,
+                stride=2,
                 padding=1,
                 bias=self.bias,
             ),
-            nn.MaxPool2d(kernel_size=(2, 2)),
             nn.BatchNorm2d(layer_param * 10),
             self.activation,
-            nn.Dropout(self.dropout),
         )
+        self.conv_layers.append(self.conv2d6)
 
-        # Layer - Fully connected layer (10*layer_param, 16, 16) -> (10*layer_param)
+        # LAYER - Fully connected layer (10*layer_param, 4, 4) -> (10*layer_param)
         self.fc1 = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(layer_param * 10 * 16 * 16, layer_param * 10),
+            nn.Linear(layer_param * 10 * 4 * 4, layer_param * 10),
             nn.BatchNorm1d(layer_param * 10),
             self.activation,
             nn.Dropout(self.dropout),
         )
+        self.fc_layers.append(self.fc1)
 
-        # Layer - Fully connected layer (10*layer_param) -> (layer_param)
+        # LAYER - Fully connected layer (10*layer_param) -> (layer_param)
         self.fc2 = nn.Sequential(
             nn.Linear(layer_param * 10, layer_param),
             nn.BatchNorm1d(layer_param),
             self.activation,
             nn.Dropout(self.dropout),
         )
+        self.fc_layers.append(self.fc2)
 
-        # Layer -output layer (layer_param) -> (1)
+        # LAYER -output layer (layer_param) -> (1)
         self.output = nn.Sequential(
             nn.Linear(layer_param, 1),
             self.output_activation,
         )
+        self.fc_layers.append(self.output)
 
     def forward(self, X):
-        for layer in [
-            self.conv2d1,
-            self.conv2d2,
-            self.conv2d3,
-            self.conv2d4,
-            self.fc1,
-            self.fc2,
-        ]:
+        for layer in self.conv_layers:
             X = layer(X)
-        return self.output(X)
+        for layer in self.fc_layers:
+            X = layer(X)
+        return X
