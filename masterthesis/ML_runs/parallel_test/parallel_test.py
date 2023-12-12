f09ee85af62c5ddd27cb9c6d7deaa87972ba85a4
Johanmkr <johan.m.kroken@gmail.com>
2023-12-12 12:37:45 +0100

    script for distributed parallel training ready to be tested

--- masterthesis/ML_runs/parallel_test/parallel_test.py
+++ masterthesis/ML_runs/parallel_test/parallel_test.py
@@ -18,6 +18,7 @@ import torch.nn as nn
 from torch.nn.parallel import DistributedDataParallel as DDP
 import numpy as np
 import random
+import pandas as pd
 
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir, os.pardir))
 sys.path.append(parent_dir)
@@ -50,6 +51,16 @@ dropout = 0.5
 lr = 0.001
 betas = (0.9, 0.999)
 weight_decay = 1e-5
+max_epochs = 100
+best_loss = float("inf")
+breakout_loss = 1e-5
+
+# Logging and saving
+writer_path = "runs/parallel_test"
+model_save_path = "models/parallel_test.pt"
+
+# Print summary of all variables as one string
+print(f"Variables:\n{pd.DataFrame(globals().items(), columns=['Parameter', 'Value'])}")
 
 ###############################################################
 ######### LOAD DATA ###########################################
@@ -93,12 +104,16 @@ model = PENGUIN(
     bias=bias,
     dropout=dropout,
 )
+print(f"Model:\n{model}")
 
 
 ###############################################################
 ######### GPU stuff ###########################################
 GPU = torch.cuda.is_available()
 world_size = torch.cuda.device_count()
+
+print("GPU: ", GPU)
+print("World size: ", world_size)
 
 
 def setup(rank, world_size):
@@ -117,13 +132,50 @@ def cleanup():
 ######### TRAINING (PARALLEL) #################################
 
 
-def train(rank, world_size, train_dataset, model):
+def success(output, label):
+    tol = 1e-2
+    return (abs(output - label) < tol).sum().item()
+
+
+def evaluate(model, rank, loss_fn, test_loader):
+    model.eval()
+    test_loss = 0
+    correct_guesses = 0
+    nr_samples = len(test_loader.dataset)
+    with torch.no_grad():
+        for batch in test_loader:
+            images, labels = batch["image"], batch["label"]
+            images = images.to(rank)
+            labels = labels.to(rank)
+
+            outputs = model(images)
+            loss = loss_fn(outputs, labels)
+
+            # Print statistics
+            test_loss += loss.item()
+            correct_guesses += success(outputs, labels)
+    test_accuracy = correct_guesses / nr_samples
+    print(
+        f"Predicted (test set): [{correct_guesses}/{len(nr_samples)}] => {test_accuracy*100} %"
+    )
+    return test_loss, test_accuracy
+
+
+def train(rank, world_size, train_dataset, test_dataset, model):
     setup(rank, world_size)
 
     # Make dataloaders
     print("Making dataloaders...")
     train_dataloader = DataLoader(
         train_dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        prefetch_factor=prefetch_factor,
+        shuffle=True,
+        pin_memory=True,
+    )
+    test_dataloader = DataLoader(
+        test_dataset,
         batch_size=batch_size,
         num_workers=num_workers,
         prefetch_factor=prefetch_factor,
@@ -139,10 +191,18 @@ def train(rank, world_size, train_dataset, model):
         ddp_model.parameters(), lr=lr, betas=betas, weight_decay=weight_decay
     )
 
+    # Writer
+    if rank == 0:
+        writer = SummaryWriter(writer_path)
+
     # Train model
-    for epoch in range(1):
-        for batch in train_dataloader:
-            print(f"Rank {rank} got batch {batch}")
+    for epoch in range(max_epochs):
+        epoch_loss = 0
+        correct_guesses = 0
+        total_guesses = 0
+        nr_batches = len(train_dataloader)
+        for i, batch in enumerate(train_dataloader):
+            # print(f"Rank {rank} got batch {i+1} / {nr_batches}")
             images, labels = batch["image"], batch["label"]
             images = images.to(rank)
             labels = labels.to(rank)
@@ -150,13 +210,109 @@ def train(rank, world_size, train_dataset, model):
             optimizer.zero_grad()
             outputs = ddp_model(images)
             loss = loss_fn(outputs, labels)
+            epoch_loss += loss.item()
             loss.backward()
             optimizer.step()
-            
-            # Print statistics
-            if 
+            correct_guesses += success(outputs, labels)
+            total_guesses += labels.size(0)
+
+        # Calculate rank statistics and send to rank 0
+        accuracy_tensor = torch.tensor(
+            [correct_guesses, total_guesses], dtype=torch.float
+        ).to(rank)
+        loss_tensor = torch.tensor([epoch_loss]).to(rank)
+        reduced_accuracy = [torch.zeros(2).to(rank) for _ in range(world_size)]
+        reduced_loss = [torch.zeros(1).to(rank) for _ in range(world_size)]
+        dist.all_gather(reduced_loss, loss_tensor)
+        dist.all_gather(reduced_accuracy, accuracy_tensor)
+
+        # Evaluate
+        test_loss, test_accuracy = evaluate(
+            ddp_model.module, rank, loss_fn, test_dataloader
+        )
+
+        # Placeholder for early stopping across processes
+        should_stop = torch.tensor(False, dtype=torch.bool).to(rank)
+
+        # Calculate and log statistics on rank 0
+        if rank == 0:
+            mean_loss = torch.stack(reduced_loss).mean().item()
+            total_correct, total_samples = (
+                torch.stack(reduced_accuracy).sum(dim=0).item()
+            )
+            mean_accuracy = total_correct / total_samples
+            print(
+                f"Epoch {epoch+1} / {max_epochs}: Loss: {mean_loss}, Accuracy: {mean_accuracy}"
+            )
+            # Write to tensorboard
+            writer.add_scalars("Loss", {"train": mean_loss, "test": test_loss}, epoch)
+            writer.add_scalars(
+                "Accuracy",
+                {"train": mean_accuracy, "test": test_accuracy},
+                epoch,
+            )
+            if epoch % 10 == 0:
+                writer.flush()
+
+            # Early stopping condition
+            if mean_loss < breakout_loss:
+                should_stop = torch.tensor(True, dtype=torch.bool).to(rank)
+
+        # Distribute early stopping across all processes
+        dist.broadcast(should_stop, src=0)
+
+        # Save model if mean loss is better than best loss or if early stopping
+        if rank == 0:
+            if should_stop or mean_loss < best_loss:
+                best_loss = mean_loss
+                print(f"New best loss found: {best_loss} Saving model...")
+
+                torch.save(
+                    {
+                        "epoch": epoch,
+                        "model_state_dict": model.state_dict(),
+                        "optimizer_state_dict": optimizer.state_dict(),
+                        "loss": mean_loss,
+                    },
+                    model_save_path,
+                )
+        # break all processes if early stopping
+        if should_stop.item():
+            print(f"Rank {rank} breaking...")
+            break
+
+        # Synchronize
+        dist.barrier()
+    print(f"Trained for {epoch+1} epochs") if rank == 0 else None
+    print(f"Rank {rank} finished training...")
+
+    # Closing writer and cleaning up
+    if rank == 0:
+        writer.flush()
+        writer.close()
 
     cleanup()
 
 
 ###############################################################
+######### MAIN ################################################
+
+
+def main():
+    torch.multiprocessing.spawn(
+        train,
+        args=(
+            world_size,
+            train_dataset,
+            test_dataset,
+            model,
+        ),
+        nprocs=world_size,
+        join=True,
+    )
+
+
+###############################################################
+
+if __name__ == "__main__":
+    main()
