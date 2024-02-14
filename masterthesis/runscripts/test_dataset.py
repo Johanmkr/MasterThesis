a37e6a8827b8d1110336a49d52ff7e7ac8fa9c37
Johanmkr <johan.m.kroken@gmail.com>
2024-02-14 17:00:31 +0100

    updt

--- masterthesis/runscripts/test_dataset.py
+++ masterthesis/runscripts/test_dataset.py
@@ -8,12 +8,15 @@ import sys
 parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 sys.path.append(parent_dir)
 
-from src.data import transforms, custom_dataset
+from src.data import calc_mean_std as cms
 
 from IPython import embed
 
 
-norm = transforms.Normalise(redshifts=[1.0])
-# ds = custom_dataset.CustomDataset(transform=None, additional_info=True)
-
-# embed()
+if __name__ == "__main__":
+    print("Calculating mean")
+    redshift = float(input("Redshift (0,1,5,10,15,20): "))
+    if input("Save statistics? (y/n): ") == "y":
+        cms.save_statistics(redshift)
+    else:
+        print("Not saving statistics.")
